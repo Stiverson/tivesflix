86849508994fd8e62497016b6d5378e769761e7b
Stiverson <stiverson.machado@gmail.com>
2020-07-29 14:07:08 -0300

    Setup inicial do projeto

---
+++ src/componentes/Menu/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import'./Menu.css';
+import Button from '../Button'
+//import ButtonLink from '../componentes/ButtonLink';
+
+
+function Menu() {
+
+    return(
+        <nav className="Menu">
+           <a href="/">
+             <img className="Logo" src={Logo} alt='tivesflix logo'/>
+           </a>
+
+            <Button as="a" className="ButtonLink" href="/">
+                Novo Conteudo
+            </Button>
+        </nav>
+
+    );
+}
+
+export default Menu;
