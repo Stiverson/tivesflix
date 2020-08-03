a021884a0537104c332f75df984c53fb4c7667de
Stiverson <stiverson.machado@gmail.com>
2020-08-03 13:36:23 -0300

    alteraçao 8

--- src/index.js
+++ src/index.js
@@ -6,9 +6,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
+// novo commit
 
-// Desafio master blaster na descrição
-// Colocar um jogo ou brincadeira nessa página:
 const Pagina404 = () => (
   <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
     <h1>Ops, quebrou tudo :O</h1>
