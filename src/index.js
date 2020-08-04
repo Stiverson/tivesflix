83ff567f9b136add33190ed26032cdc4dac46dc3
Stiverson <stiverson.machado@gmail.com>
2020-08-04 19:11:09 -0300

    modificação footer 3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/index.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from '../src/pages/home';
+import Home from './pages/home';
 import CadastroVideo from './pages/cadastro/video';
 import CadastroCategoria from './pages/cadastro/categoria';
 
