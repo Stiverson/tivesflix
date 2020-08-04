5c0128ec7fea2dfaf8e55f05a6b9d5eeb49361f4
Stiverson <stiverson.machado@gmail.com>
2020-08-04 20:56:07 -0300

    mdf45

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,7 @@ import Menu from '../../components/Menu'
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import Footer from '../../components/footer';
+import Footer from '../../components/Footer';
 
 function Home() {
   return (
