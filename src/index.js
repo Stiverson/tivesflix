d13ab3b821c84282d1193e4c393431f46befbed3
Stiverson <stiverson.machado@gmail.com>
2020-08-06 12:36:51 -0300

    Primeiro update aula 5

--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/index.css';
+
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CadastroVideo from './pages/Cadastro/Video';
 import CadastroCategoria from './pages/Cadastro/Categoria';
 
-const Pagina404 = () => (
-  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-    <h1>Ops, quebrou tudo :O</h1>
-    <p>
-      <a href="/">Você pode jogar ou voltar pra home :)</a>
-    </p>
-    <p>
-      Ou <a href="https://www.youtube.com/watch?v=jOAU81jdi-c&list=PLTcmLKdIkOWmeNferJ292VYKBXydGeDej">aprender a fazer o jogo</a>
-    </p>
-    {/*
-      Pessoal, quem quiser fazer o desafio do Flappy Bird, da pra usar esse iframe aqui:
-      - https://codepen.io/omariosouto/pen/pogmdGE
+const Pagina404 = () => (<div>Página 404</div>)
 
-      E quem quiser programar o jogo:
-      - https://www.youtube.com/watch?v=jOAU81jdi-c&list=PLTcmLKdIkOWmeNferJ292VYKBXydGeDej
-    */}
-    <iframe
-      title="Flappy Bird Game"
-      src="https://mariosouto.com/flappy-bird-devsoutinho/"
-      width="340"
-      height="600" />
-  </div>
-)
 
 ReactDOM.render(
   <BrowserRouter>
