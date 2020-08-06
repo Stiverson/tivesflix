2a1b1eae11c9baf01880d30d27b433edabb8bcc0
Stiverson <stiverson.machado@gmail.com>
2020-08-05 22:03:55 -0300

    Finalização da aula 4

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,8 +1,10 @@
+/* eslint-disable linebreak-style */
 import styled from 'styled-components';
 
 const Button = styled.button`
 color: var(--white);
 border: 1px solid var(--white);
+background : var(--black);
 box-sizing: border-box;
 cursor: pointer;
 padding: 16px 24px;
@@ -22,4 +24,4 @@ opacity: .5;
 
 `;
 
-export default Button
+export default Button;
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,24 +1,23 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
-import'./Menu.css';
-import Button from '../Button'
-//import ButtonLink from '../componentes/ButtonLink';
-
+import './Menu.css';
+import Button from '../Button';
 
 function Menu() {
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="tivesflix logo" />
+      </a>
 
-    return(
-        <nav className="Menu">
-           <a href="/">
-             <img className="Logo" src={Logo} alt='tivesflix logo'/>
-           </a>
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo Conteudo
+      </Button>
+    </nav>
 
-            <Button as="a" className="ButtonLink" href="/">
-                Novo Conteudo
-            </Button>
-        </nav>
-
-    );
+  );
 }
 
 export default Menu;
